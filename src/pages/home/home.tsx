bd86fb75180f30f258cb11e0a538a75b070ed4f2
Tomas Martinez <tomas.martinez@xmartlabs.com>
2022-11-02 13:54:44 -0300

    Pagination with query parameters

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,32 +2,77 @@ import React, { useEffect, useState } from 'react';
 
 import globalStyles from 'assets/stylesheets/global-styles.module.scss';
 import { UserController } from 'networking/controllers/user-controller';
+import { ParamsHelper } from 'helpers/params-helper';
 import styles from './home.module.scss';
+
+const getPage = () => {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  const page = params.get('page');
+  if (!page || Number(page) < 0) {
+    return 0;
+  }
+  return Number(page);
+};
 
 const Home = () => {
   const [users, setUsers] = useState<DummyUser[]>([]);
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(getPage());
   const [lastPage, setLastPage] = useState(0);
   const [disable, setDisable] = useState(false);
 
   const RECORDS_PER_PAGE = 20;
 
+  const setParams = (defaultPage? : boolean) => {
+    const queryPage = getPage();
+    let params;
+    if (defaultPage) {
+      params = '?page=0';
+    } else {
+      params = ParamsHelper.createQueryParams({ page: !queryPage ? currentPage : queryPage });
+    }
+    const route = `/${params}`;
+    window.history.pushState({}, '', route);
+  };
+
+  const redirectDefaultPage = () => {
+    const params = '?page=0';
+    const route = `/${params}`;
+    window.history.pushState({}, '', route);
+    setCurrentPage(0);
+  };
+
+  const updateParams = (step : number) => {
+    const params = ParamsHelper.createQueryParams({ page: currentPage + step });
+    const route = `/${params}`;
+    window.history.pushState({}, '', route);
+  };
+
   const handleNextPageClick = () => {
+    updateParams(1);
     setCurrentPage(currentPage + 1);
   };
 
   const handlePreviousPageClick = () => {
+    updateParams(-1);
     setCurrentPage(currentPage - 1);
   };
 
   useEffect(() => {
     setDisable(true);
-    UserController.getUsers(currentPage).then((value) => {
-      setUsers(value.users);
-      setLastPage(Math.floor(value.total / RECORDS_PER_PAGE));
-      setDisable(false);
+    const page = getPage();
+    UserController.getUsers(page).then((value) => {
+      if (value.users.length === 0) {
+        redirectDefaultPage();
+      } else {
+        setUsers(value.users);
+        setLastPage(Math.floor(value.total / RECORDS_PER_PAGE));
+        setDisable(false);
+        setParams();
+      }
     });
   }, [currentPage]);
+
   return (
     <div className={globalStyles.genericContainer}>
       <h1 className={styles.title}>
@@ -39,7 +84,7 @@ const Home = () => {
       {users.length > 0 && (
         <div className={globalStyles.genericItemContainer}>
           {users.map((user) => (
-            <div className={styles.userCard} data-testid="userCard">
+            <div key={user.id} className={styles.userCard} data-testid="userCard">
               <img
                 src={user.picture}
                 alt="User profile"
@@ -50,7 +95,7 @@ const Home = () => {
           ))}
         </div>
       )}
-      {(currentPage > 0) && (
+      {(currentPage > 0 && users.length !== 0) && (
         <button
           disabled={disable}
           type="button"
