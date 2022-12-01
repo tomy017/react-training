e214c2295d8ffca857a834dd1b8be2558f2bbda4
Tomas Martinez <tomas.martinez@xmartlabs.com>
2022-12-01 10:25:23 -0300

    Port added to constants file

--- src/networking/api-routes.ts
+++ src/networking/api-routes.ts
@@ -2,10 +2,14 @@
   Add all API paths used on the app here so that
   they are centrally documented.
 */
+import { constants } from 'config/constants';
+
+const port = constants.apiPort;
+
 const API_ROUTES = {
   USERS: 'https://dummyapi.io/data/v1/user',
-  SIGNUP: 'http://localhost:3001/signup',
-  LOGIN: 'http://localhost:3001/login',
+  SIGNUP: `http://localhost:${port}/signup`,
+  LOGIN: `http://localhost:${port}/login`,
 };
 
 export { API_ROUTES };
